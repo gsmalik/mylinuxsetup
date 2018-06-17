4b840585a386170019482734b090b9a8bfef2114
gsmalik <gsmalik@uwaterloo.ca>
2018-06-17 13:18:42 -0400

    disble entwork manager in eduroam

--- eduroam_connect.sh
+++ eduroam_connect.sh
@@ -1,3 +1,4 @@
+sudo systemctl stop NetworkManager.service 
 sudo rm -rf /var/run/wpa_supplicant/wlx00e04c13211e
 sudo wpa_supplicant -i wlx00e04c13211e -D wext -c /etc/wpa_supplicant/wpa_supplicant.conf &
 sleep 20
